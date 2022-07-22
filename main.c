123026d854ead52f17f33e04908308c7be61eb7a
huubexel <huubexel@hotmail.com>
2022-07-22 23:16:30 +0200

    First for loop

--- main.c
+++ main.c
@@ -2,16 +2,9 @@
 
 int main()
 {
-    float fahrenheit, celsius;
-    int upper, step;
+    int fahr;
 
-    upper = 300;
-    step = 20;
-
-    printf("Fahrenheit   Celsius");
-    while (fahrenheit <= upper) {
-        celsius = (5.0/9.0) * (fahrenheit-32.0);
-        printf("%9.0f %6.1f\n", fahrenheit, celsius);
-        fahrenheit = fahrenheit + step;
-    }
+    printf("Fahrenheit   Celsius\n");
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+        printf("%10d   %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
