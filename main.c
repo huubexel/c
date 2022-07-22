76bd74a3109ebab092651d3b1769fb0e54b43890
huubexel <huubexel@hotmail.com>
2022-07-22 23:06:13 +0200

    First working program

--- main.c
+++ main.c
@@ -5,9 +5,13 @@ int main()
     float fahrenheit, celsius;
     int upper, step;
 
+    upper = 300;
+    step = 20;
+
+    printf("Fahrenheit   Celsius");
     while (fahrenheit <= upper) {
         celsius = (5.0/9.0) * (fahrenheit-32.0);
-        printf("%3.0f %6.1f\n", fahrenheit, celsius);
+        printf("%9.0f %6.1f\n", fahrenheit, celsius);
         fahrenheit = fahrenheit + step;
     }
 }
