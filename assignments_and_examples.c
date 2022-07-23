f42b521a5e876d7e8f3a2aeef38460580e566d4b
huubexel <huubexel@hotmail.com>
2022-07-23 20:30:18 +0200

    1.12 and example

--- assignments_and_examples.c
+++ assignments_and_examples.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define UPPER 300
 #define STEP 20
@@ -65,9 +66,7 @@ int count_more()
     // 1.8
     int c, blanks, tabs, newlines;
 
-    blanks = 0;
-    tabs = 0;
-    newlines = 0;
+    blanks = tabs = newlines = 0;
 
     while ((c = getchar()) != EOF) {
         if (c == '\n')
@@ -126,6 +125,99 @@ int replace_real_characters()
     }
 }
 
+//int word_per_line()
+//{
+//     1.12
+//     Will figure this out further when I know how an array works
+//     Or something more usefull.
+//    strcat concetenates two strings
+//     string_name[], maak je een string mee.
+//    int c;
+//    char string[] = "";
+//
+//    while ((c = getchar()) != EOF) {
+//        if (c == ' ' || c == '\n' || c == '\t') {
+//            printf("%s\n", string);
+//            memset(string,0,strlen(string));
+//        } else {
+//            char d = c + '0';
+//            strcat(string, d);
+//        }
+//    }
+//}
+
+
+int word_per_line_2()
+{
+    int c, i, counter;
+    int word_array[99]; /* make array for one word at the time */
+
+    counter = 0;
+
+    while ((c = getchar()) != EOF) {
+        word_array[counter] = c;
+        ++counter;
+        if (c == '\n') {
+            for (i = 0; word_array[i] != '\0'; ++i) {
+                if (word_array[i] == ' ')
+                    printf("\n");
+                else
+                    printf("%c", word_array[i]); /* print character */
+                word_array[i] = '\0'; /* empty this index of array */
+            }
+            printf("\n");
+            counter = 0; /* set counter to 0, cause the word is printed */
+        }
+    }
+}
+
+int d_w_o_counter()
+{
+    /* c is input,
+     * i is an array,
+     * nwhite is number of whitespaces, tabs, newlines
+     * nother is number of other characters (letters) */
+    int c, i, nwhite, nother;
+    int ndigit[10]; /* array */
+
+    nwhite = nother = 0;
+    for (i = 0; i < 10; ++i)
+        /* fill the array with 10 zero's */
+        ndigit[i] = 0;
+
+    while ((c = getchar()) != EOF)
+        /* if the input character is a number */
+        if (c >= '0' && c <= '9')
+            /* c-'0' is the numeric value of input
+             * numeric value of the digit. */
+            ++ndigit[c-'0'];
+        else if (c == ' ' || c == '\n' || c == '\t')
+            ++nwhite;
+        else
+            ++nother;
+
+    /*
+     * A little explanation to myself here:
+     * c is the input, this is a number (an ASCII number)
+     * let's say the input to '5' is ASCII number 63
+     * than '6' is 64, '7' is 65 etc.
+     * If you subtract '0' which would according to this
+     * example be 58 as ASCII number, say the input is 5
+     * which for the machine is 63, now you don't want to
+     * update the 63th (or 64th because you start at 0)
+     * index of the array, you want to update the 5th.
+     * How do you get 5 from '5', well if you substract
+     * '0' from '5' you get this equation: 63 - 58 = 5
+     * which is the index you want, this is how it works.
+     */
+
+    printf("digits = ");
+    for (i = 0; i < 10; ++i)
+        printf(" %d", ndigit[i]);
+    printf(", white space = %d, other = %d\n", nwhite, nother);
+}
+
+
 int main () {
     //fahrenheit();
     //print_input();
@@ -134,5 +226,7 @@ int main () {
     //count_lines();
     //count_more();
     //replace_multiple_blanks_with_single();
-    replace_real_characters();
+    //replace_real_characters();
+    //word_per_line_2();
+    //d_w_o_counter();
 }
