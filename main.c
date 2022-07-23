ce1a79c2b9d5a9b978fa201c90ed91d7a78c8ca0
huubexel <huubexel@hotmail.com>
2022-07-23 14:09:02 +0200

    Reshaped to one file so I can run it in Clion

--- main.c
+++ main.c
@@ -4,11 +4,32 @@
 #define STEP 20
 #define LOWER 0
 
-int main()
-{
+void fahrenheit();
+void print_input();
+
+int main () {
+    fahrenheit();
+    //print_input();
+
+}
+
+void fahrenheit() {
     int fahr;
 
     printf("Fahrenheit   Celsius\n");
     for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%10d   %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
+
+void print_input() {
+    int c;
+    int check;
+
+    while ((c = getchar()) != EOF) {
+        putchar(c);
+    }
+
+    check = (getchar() != EOF);
+    printf("\n%d\n", EOF);
+    printf("%d\n", check);
 }
