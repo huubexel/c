6d8a8b143c8f9c8742742494702948399628be33
huubexel <huubexel@hotmail.com>
2022-09-10 10:13:45 +0200

    First assignment hello.c and first push

---
+++ impprog/week1/hello/hello.c
@@ -0,0 +1,14 @@
+/* file: hello.c */
+/* author: Huub Exel (email: h.exel@student.rug.nl) */
+/* date: Sat Sep 10 2022 */
+/* version: 1.0 */
+/* Description: This program prints ’Hello world’ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello world!\n");
+    return 0;
+}
