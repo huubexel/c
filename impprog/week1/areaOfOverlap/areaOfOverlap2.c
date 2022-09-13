eed6fa914c416649e3e4dfe1895cc60f125eeb8a
huubexel <huubexel@hotmail.com>
2022-09-13 21:55:36 +0200

    I got it to work, I will refractor some of the code and then it's done

--- impprog/week1/areaOfOverlap/areaOfOverlap2.c
+++ impprog/week1/areaOfOverlap/areaOfOverlap2.c
@@ -1,6 +1,6 @@
 /* file: areaOfOverlap.c */
 /* author: Huub Exel (email: h.exel@student.rug.nl) */
-/* date: Sat Sep 11 2022 */
+/* date: Sat Sep 13 2022 */
 /* version: 1.0 */
 
 /* Description:
@@ -10,24 +10,75 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Voor het compleet omcirkelen moet je wat anders maken
-
 int main(int argc, char *argv[]) {
-    int x, y;
-    x = 0;
-    y = 0;
+    /* Input */
+    int int1, int2, int3, int4, int5, int6, int7, int8;
+    scanf("%d %d %d %d", &int1, &int2, &int3, &int4);
+    scanf("%d %d %d %d", &int5, &int6, &int7, &int8);
 
-    int calc1, calc2, calc3, calc4;
-
+    /* For my equation I will need the two points to be the bottom-left point
+     * and the top-right point, this will make sure that is the case */
     int x1Rect1, y1Rect1, x2Rect1, y2Rect1;
     int x1Rect2, y1Rect2, x2Rect2, y2Rect2;
-    scanf("%d %d %d %d", &x1Rect1, &y1Rect1, &x2Rect1, &y2Rect1);
-    scanf("%d %d %d %d", &x1Rect2, &y1Rect2, &x2Rect2, &y2Rect2);
+    if (int1 > int3) {
+        x2Rect1 = int1;
+        x1Rect1 = int3;
+    } else {
+        x2Rect1 = int3;
+        x1Rect1 = int1;
+    }
 
-    // The x part is completely finished and works every way possible!
+    if (int2 > int4) {
+        y2Rect1 = int2;
+        y1Rect1 = int4;
+    } else {
+        y2Rect1 = int4;
+        y1Rect1 = int2;
+    }
+
+    if (int5 > int7) {
+        x2Rect2 = int5;
+        x1Rect2 = int7;
+    } else {
+        x2Rect2 = int7;
+        x1Rect2 = int5;
+    }
+
+    if (int6 > int8) {
+        y2Rect2 = int6;
+        y1Rect2 = int8;
+    } else {
+        y2Rect2 = int8;
+        y1Rect2 = int6;
+    }
+
+    /* The rectangle, which lowest x is higher than the other rectangles'
+     * lowest x, has to be rectangle1 in my equation, if that is not the case
+     * switch the two rectangles */
+    if (x1Rect1 < x1Rect2) {
+        int temp1, temp2, temp3, temp4;
+        temp1 = x1Rect2;
+        temp2 = y1Rect2;
+        temp3 = x2Rect2;
+        temp4 = y2Rect2;
+        x1Rect2 = x1Rect1;
+        y1Rect2 = y1Rect1;
+        x2Rect2 = x2Rect1;
+        y2Rect2 = y2Rect1;
+        x1Rect1 = temp1;
+        y1Rect1 = temp2;
+        x2Rect1 = temp3;
+        y2Rect1 = temp4;
+    }
+
+    printf("%d %d %d %d\n", x1Rect1, y1Rect1, x2Rect1, y2Rect1);
+    printf("%d %d %d %d\n", x1Rect2, y1Rect2, x2Rect2, y2Rect2);
+
+
+    int x, y;
+    int calc1, calc2, calc3, calc4;
     if (x1Rect2 > x2Rect1 || x2Rect2 < x1Rect1) {
         x = 0;
-        printf("het is x\n");
     } else {
         if (x2Rect1 < x2Rect2) {
             calc1 = x2Rect1;
@@ -39,6 +90,7 @@ int main(int argc, char *argv[]) {
         } else {
             calc2 = x1Rect2;
         }
+        printf("calc1 %d calc2 %d\n\n", calc1, calc2);
         x = calc1 - calc2;
     }
 
@@ -48,32 +100,27 @@ int main(int argc, char *argv[]) {
         y = 0;
         printf("het is y\n");
     } else {
-        if (x2Rect1 < y2Rect2) {
-            calc3 = x2Rect1;
+        if (y2Rect1 < y2Rect2) {
+            calc3 = y2Rect1;
         } else {
             calc3 = y2Rect2;
         }
 
-        if (y1Rect1 < x2Rect2) {
+        if (y1Rect1 > y1Rect2) {
             calc4 = y1Rect1;
         } else {
             calc4 = x2Rect2;
         }
 
-        printf("calc3 %d calc4 %d\n", calc3, calc4);
-        if (calc4 < 0) {
-            y = calc3 + calc4;
-        } else {
-            y = calc3 - calc4;
-        }
+        // wat je uit moet rekenen is de afstand van het lijntje
+        // dus stel de lijn loopt van 1 tm -2 dan wordt het dus 1 - - 2
+        printf("calc3 %d calc4 %d\n\n", calc3, calc4);
 
-        if (y < 0) {
-            y = y * -1;
-        }
+        y = calc3 - calc4;
     }
 
     int a = x * y;
 
-    printf("%d", a);
+    printf("%d\n", a);
 
 }
