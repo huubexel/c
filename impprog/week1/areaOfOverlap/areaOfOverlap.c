1c56e8c15cb18c668aecc6594fbdfc84cf39eeb9
Huub Exel <huubexel@hotmail.com>
2022-09-16 22:37:31 +0200

    I think I figured it out, I set some really clear rules with myself and I had to follow them and it seems to work now

--- impprog/week1/areaOfOverlap/areaOfOverlap.c
+++ impprog/week1/areaOfOverlap/areaOfOverlap.c
@@ -6,17 +6,6 @@
 /* Description:
  */
 
-// Als 1 van de vierkanten in de ander zit, dan gaat de kleinste bovenaan de calculatie
-
-// Dus stel de y waarde van beide is hetzelfde (de x-lijnen zijn op dezelfde hoogtes),
-// Dan is diegene die bovenaan moet is degene met de kleinste x-waarde
-// Dit geld ook als de x waarde van beide hetzelfde is.
-
-
-// Edge cases:
-// 1. ze hebben precies hetzelfde startpunt. Bijvoorbeeld allebei 0,0
-// 2. 2 points of 1 rect are in another rect en dit links of onder. 1 groter dan de ander.
-// 3. de x-ass of de y-ass die onder of links is komt overeen voor beide vierkanten.
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -27,8 +16,7 @@ int main(int argc, char *argv[]) {
     scanf("%d %d %d %d", &int1, &int2, &int3, &int4);
     scanf("%d %d %d %d", &int5, &int6, &int7, &int8);
 
-    /* How to read these 8 variables:
-     * x1Rec1 --> can be deciphered in 3 parts (this works for all 8 of these variables):
+    /* How to read these 8 variables, for example x1Rec1:
      * x --> x-ass value for this point, y being the y-ass value for this point
      * 1 --> bottom-left point, 2 being the top-right point
      * Rec1 --> from rectangular 1, Rec2 being from rectangular 2 */
@@ -73,10 +61,8 @@ int main(int argc, char *argv[]) {
         y1Rec2 = int6;
     }
 
-    /* The rectangle, which lowest x is higher than the other rectangles'
-     * lowest x, has to be rectangle1 in my equation, if that is not the case
-     * this will switch the two rectangles */
-    if (x1Rec1 < x1Rec2 || (x1Rec1 == x1Rec2 && (y2Rec2 < y2Rec1 || x2Rec2 < x2Rec1))) {
+    // hier dat stukje wat ik heb afgesproken met mezelf neerzetten.
+    if (y1Rec2 < y1Rec1 || (y1Rec2 == y1Rec1 && x1Rec2 < x1Rec1)) {
         int temp1, temp2, temp3, temp4;
         temp1 = x1Rec2;
         temp2 = y1Rec2;
@@ -96,49 +82,45 @@ int main(int argc, char *argv[]) {
     printf("%d %d %d %d\n", x1Rec2, y1Rec2, x2Rec2, y2Rec2);
 
 
-    // Wat er dan fout gaat, is dat ze allebei dezelfde y hebben, die lijnen.
-
+    /* overlapX1 is the left-under x point of overlapping etc*/
     int x, y;
-    int endLineX, startLineX, topLineY, bottomLineY;
+    int overlapX1, overlapX2, overlapY1, overlapY2;
 
     if (x1Rec2 > x2Rec1 || x2Rec2 < x1Rec1) {
         x = 0;
     } else {
-        if (x2Rec1 < x2Rec2) {
-            endLineX = x2Rec1;
-        } else {
-            endLineX = x2Rec2;
-        }
         if (x1Rec1 > x1Rec2) {
-            startLineX = x1Rec1;
+            overlapX1 = x1Rec1;
         } else {
-            startLineX = x1Rec2;
+            overlapX1 = x1Rec2;
         }
-        printf("endLineX %d startLineX %d\n\n", endLineX, startLineX);
-        x = endLineX - startLineX;
+        if (x2Rec1 < x2Rec2) {
+            overlapX2 = x2Rec1;
+        } else {
+            overlapX2 = x2Rec2;
+        }
+        x = overlapX1 - overlapX2;
     }
 
 
     if (y1Rec2 > y2Rec1 || y2Rec2 < y1Rec1) {
         y = 0;
-        printf("het is y\n");
     } else {
         if (y2Rec1 < y2Rec2) {
-            topLineY = y2Rec1;
+            overlapY2 = y2Rec1;
         } else {
-            topLineY = y2Rec2;
+            overlapY2 = y2Rec2;
         }
 
-        if (y1Rec1 > y1Rec2) {
-            bottomLineY = y1Rec1;
-        } else {
-            bottomLineY = x2Rec2;
-        }
+        /* Because I made sure to always put the rectangle with the lowest
+         * y-point as rectangle1, this is always the case */
+        overlapY1 = y1Rec2;
 
-        printf("topLineY %d bottomLineY %d\n\n", topLineY, bottomLineY);
-
-        y = topLineY - bottomLineY;
+        y = overlapY1 - overlapY2;
     }
+
+    printf("point1 (%d, %d)\n", overlapX1, overlapY1);
+    printf("point2 (%d, %d)\n", overlapX2, overlapY2);
 
     int a = x * y;
 
