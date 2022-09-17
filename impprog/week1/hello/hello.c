22769a353e1e83b6c97d679dacb9af91d243ba18
Huub Exel <huubexel@hotmail.com>
2022-09-17 20:06:28 +0200

    Adjusted to style guide

--- impprog/week1/hello/hello.c
+++ impprog/week1/hello/hello.c
@@ -2,6 +2,7 @@
 /* author: Huub Exel (email: h.exel@student.rug.nl) */
 /* date: Sat Sep 10 2022 */
 /* version: 1.0 */
+
 /* Description: This program prints 'Hello world' */
 
 #include <stdio.h>
@@ -9,6 +10,6 @@
 #include <math.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello world!\n");
+    printf("Hello world!\n"); /* Prints Hello world! */
     return 0;
 }
