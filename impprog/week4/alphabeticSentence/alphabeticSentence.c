bb7a2e3e66bf7b6dff52b34effad0f64f2435e84
Huub Exel <huubexel@hotmail.com>
2022-10-11 13:35:52 +0200

    Is alphabeticSentence.c finished

--- impprog/week4/alphabeticSentence/alphabeticSentence.c
+++ impprog/week4/alphabeticSentence/alphabeticSentence.c
@@ -1,6 +1,6 @@
 /* file: alphabeticSentence.c */
 /* author: Huub Exel (email: h.exel@student.rug.nl) */
-/* date: Thu Oct 06 2022 */
+/* date: Mon Oct 11 2022 */
 /* version: 1.0 */
 
 /* Description:
@@ -11,8 +11,10 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int toLowerChar(int word) {
+    /* returns the lowered character */
     if ((((int)word) > 64) && (((int)word) < 91)) {
         return word+32;
     } else {
@@ -20,48 +22,52 @@ int toLowerChar(int word) {
     }
 }
 
+int isAlphabeticChar(int letter) {
+    /* Checks if the character given is Alphabetic */
+    if (((((int)letter) > 64) && (((int)letter) < 91)) || ((((int)letter) > 96) && (((int)letter) < 123))) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main() {
 
     int asciiCharToBeat = 0; /* asciiCharToBeat is the ascii value of the character to beat */
-    int correctSequences = 0;
 
-    /* These 2 are value 1, so it immediately takes the first character of the sentence without needing a whitespace */
-    int amountOfWords = 1;
+    /* This is 1 So it will immediately take the first character */
     int counter = 1;
 
     char c;
     while (c != '\n') { /* While loops stops when enter is pressed */
 
         c = getchar();
-        if (c == ' ') {
+        if (c == ' ') { /* If the char is a space, set counter to 0 */
             counter = 0;
-            amountOfWords += 1; /* if there is a space the amount of words goes up by 1 */
         }
 
-        /* If it is the first char of the sentence or the first char after a whitespace */
-        if (counter == 1 && (((int)c) < 128)) {
+        /* If it is the first char of the sentence or the first char after a whitespace, and it is alphabetic */
+        if (counter == 1 && isAlphabeticChar(((int)c))) {
             /* If the (lowered) character is higher in the alphabet than the character to beat */
-            if (toLowerChar(((int)c)) >= asciiCharToBeat) {
-                correctSequences += 1;
-                if (toLowerChar(((int)c)) != ((int)c)) {
-                    asciiCharToBeat = toLowerChar(c); /* uppercase characters */
-                } else {
-                    asciiCharToBeat = c; /* lowercase characters */
-                }
+            if (toLowerChar(((int)c)) > asciiCharToBeat) {
+                asciiCharToBeat = toLowerChar(c); /* the ascii value of the new character to beat is set here */
+            } else {
+                /* If it is not a correct sequence print NO */
+                printf("NO\n");
+                exit(0);
             }
         }
 
-        counter += 1;
+        if (counter == 1 && (!(isAlphabeticChar(((int)c))))) {
+            /* If the first character is not alphabetic, wait first the first one that will be alphabetic */
+            counter = 1;
+        } else {
+            counter += 1;
+        }
     }
 
-    if (correctSequences == amountOfWords) {
-        /* If the first letter of each word in the sentence
-         * starts with a letter that is further in the alphabet than the first letter of the previous word.
-         * Print YES */
-        printf("YES\n");
-    } else {
-        /* If this is not the case, print NO */
-        printf("NO\n");
-    }
+    /* If de sequence is alphabetic print YES */
+    printf("YES\n");
+
     return 0;
 }
